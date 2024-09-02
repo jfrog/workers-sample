1dfae4ac2032fea81caf996347e624d48b0628b6
Esteguet <hervee@jfrog.com>
2024-09-02 15:31:30 +0200

    Change samples folder layout

---
+++ samples/artifactory/BEFORE_DOWNLOAD/restrictDownloadByPropertyValue/worker.spec.ts
@@ -0,0 +1,26 @@
+import { PlatformContext, BeforeDownloadRequest, PlatformClients, PlatformHttpClient, DownloadStatus } from 'jfrog-workers';
+import { createMock, DeepMocked } from '@golevelup/ts-jest';
+import runWorker from './worker';
+
+describe("restrict-download-by-property-value tests", () => {
+    let context: DeepMocked<PlatformContext>;
+    let request: DeepMocked<BeforeDownloadRequest>;
+
+    beforeEach(() => {
+        context = createMock<PlatformContext>({
+            clients: createMock<PlatformClients>({
+                platformHttp: createMock<PlatformHttpClient>({
+                    get: jest.fn().mockResolvedValue({ status: 200 })
+                })
+            })
+        });
+        request = createMock<BeforeDownloadRequest>();
+    })
+
+    it('should run', async () => {
+        await expect(runWorker(context, request)).resolves.toEqual(expect.objectContaining({
+            message: 'Overwritten by worker-service if an error occurs.',
+            status: DownloadStatus.DOWNLOAD_PROCEED
+        }))
+    })
+});
